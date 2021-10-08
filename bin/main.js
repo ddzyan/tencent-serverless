2b77e29b5c775581074bbda3c701a15d79e0bb18
dingdongzhao <dingdongzhao@didiglobal.com>
2021-10-09 02:21:32 +0800

    代码结构完善

--- bin/main.js
+++
@@ -1,7 +0,0 @@
-const func = process.argv[2]
-
-if(!func) throw new Error('你必须指定函数名称')
-
-const app = require(`../functions/${func}/app`)
-
-app.listen(3000,()=>console.log('> http://localhost:3000'))
